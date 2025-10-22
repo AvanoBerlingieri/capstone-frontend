5e0181510ce09c6b6a0b56fde64aeba609ed7407
Avano <avanoberlingieri@gmail.com>
2025-10-22 17:32:16 -0400

    pages TODO

---
+++ app/src/main/java/capstone/safeline/ui/Register.kt
@@ -0,0 +1,10 @@
+package capstone.safeline.ui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class Register : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
