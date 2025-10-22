96cdb772af0755a6bacb9a6e68a9bb42cdde2689
Avano <avanoberlingieri@gmail.com>
2025-10-22 17:01:01 -0400

    updated to use compose and kotlin files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -78,7 +78,7 @@ dependencies {
     implementation("androidx.activity:activity-compose:1.11.0")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.material3:material3:1.2.0")
 
 
     val composeBom = platform("androidx.compose:compose-bom:2024.10.00")
