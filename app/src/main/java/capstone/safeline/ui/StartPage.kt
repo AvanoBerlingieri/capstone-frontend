95741050f6f6cc51d2ea11449de109f54c3299c8
Lhekdup <lhekduptenzin@gmail.com>
2026-02-14 17:39:44 -0500

    i did StartPage activity, StartScreen, Login, and Register so far

---
+++ app/src/main/java/capstone/safeline/ui/StartPage.kt
@@ -0,0 +1,50 @@
+package capstone.safeline.ui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+class StartPage : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent { SafeLineNav() }
+    }
+}
+
+@Composable
+fun SafeLineNav() {
+    val nav = rememberNavController()
+
+    NavHost(navController = nav, startDestination = "start") {
+
+        composable("start") {
+            StartScreen(
+                onLogin = { nav.navigate("login") },
+                onRegister = { nav.navigate("register") }
+            )
+        }
+
+        composable("login") {
+            LoginScreen(
+                onBack = { nav.popBackStack() },
+                onDone = { email, pass ->
+                    // TODO: call API then nav.navigate("home")
+                    // nav.navigate("home") { popUpTo("start") { inclusive = true } }
+                }
+            )
+        }
+
+        composable("register") {
+            RegisterScreen(
+                onBack = { nav.popBackStack() },
+                onDone = { username, email, pass, confirm ->
+                    // TODO: validate + call API then nav.navigate("home")
+                }
+            )
+        }
+    }
+}
---
+++ app/src/main/java/capstone/safeline/ui/theme/AppFonts.kt
@@ -0,0 +1,13 @@
+package capstone.safeline.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import capstone.safeline.R
+
+val VampiroOne = FontFamily(
+    Font(R.font.vampiro_one)
+)
+
+val KaushanScript = FontFamily(
+    Font(R.font.kaushan_script)
+)
