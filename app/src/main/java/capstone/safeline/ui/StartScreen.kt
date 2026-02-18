7f5b36237025a9a26496dcbdc1a14626224ce5ed
Nima Abady <nima.abady@gmail.com>
2026-02-17 21:01:36 -0500

    test for better fit on screen

--- app/src/main/java/capstone/safeline/ui/StartScreen.kt
+++ app/src/main/java/capstone/safeline/ui/StartScreen.kt
@@ -3,7 +3,14 @@ package capstone.safeline.ui
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
