d1141dc9adef9744ef3f2f2475e0966962c7ee38
Avano <avanoberlingieri@gmail.com>
2026-04-03 01:40:29 -0400

    Updated DB tables and query's

--- app/src/main/java/capstone/safeline/data/local/dao/MessageDao.kt
+++ app/src/main/java/capstone/safeline/data/local/dao/MessageDao.kt
@@ -13,13 +13,16 @@ interface MessageDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertMessage(message: MessageEntity)
 
-    // Using Flow so the UI updates automatically when new messages arrive
     @Query("SELECT * FROM messages WHERE receiverId = :userId OR senderId = :userId ORDER BY timestamp ASC")
     fun getMessagesForUser(userId: String): Flow<List<MessageEntity>>
 
-    @Query("UPDATE messages SET status = :newStatus WHERE messageUuid = :uuid")
+    @Query("UPDATE messages SET status = :newStatus WHERE messageId = :uuid")
     suspend fun updateMessageStatus(uuid: String, newStatus: String)
 
     @Query("SELECT * FROM messages WHERE status = 'PENDING'")
     suspend fun getUnsentMessages(): List<MessageEntity>
+
+    // grouup chats
+//    @Query("SELECT * FROM messages WHERE groupId = :groupId ORDER BY timestamp ASC")
+//    fun getMessagesForGroup(groupId: String): Flow<List<MessageEntity>>
 }
--- app/src/main/java/capstone/safeline/data/local/entity/GroupMessageEntity.kt
+++ app/src/main/java/capstone/safeline/data/local/entity/GroupMessageEntity.kt
@@ -3,11 +3,11 @@ package capstone.safeline.data.local.entity
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "messages")
-data class MessageEntity(
+@Entity(tableName = "group_messages")
+data class GroupMessageEntity(
     @PrimaryKey val messageId: String, // UUID
     val senderId: String,   // UUID
-    val receiverId: String, // UUID
+    val groupId: String,    // UUID
     val content: String,    // Text Body (String)
     val timestamp: String,  // Date
     val status: String,     // ENUM: SENT, DELIVERED
